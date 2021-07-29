1fde599c64b93a28a1d7eb98b55004d62f02899d
martins.kuznecovs <martins.kuznecovs@gmail.com>
2021-07-29 13:08:46 +0300

    second offsets

--- deepspeech_pytorch/inference.py
+++ deepspeech_pytorch/inference.py
@@ -91,7 +91,7 @@ def run_transcribe(audio_path: str,
     frames = spect.size(-1)
     print("Spect:", spect.size())
     print("Frames:", frames)
-    print(decoded_offsets)
+    decoded_offsets = decoded_offsets[0].to_numpy()
     scalar = (((frames-1)/100)/out.size(-1))
     print("Second offsets:", [i*scalar for i in decoded_offsets])
     return decoded_output, decoded_offsets
